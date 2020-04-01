63cc4ba86c9186f4d9fb961bf1e048a43470e9b4
Akshay S Dinesh <asdofindia@gmail.com>
2020-04-01 12:29:05 +0530

    feat: service worker

--- js/main.js
+++ js/main.js
@@ -211,4 +211,17 @@
     }
 
     init();
+
+  // Service Worker from https://github.com/chriscoyier/Simple-Offline-Site/blob/master/js/global.js
+  // ServiceWorker is a progressive technology. Ignore unsupported browsers
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/service-worker.js').then(function() {
+      console.log('CLIENT: service worker registration complete.');
+    }, function() {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
 })();
